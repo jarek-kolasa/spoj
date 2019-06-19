97da79177eb9fff7f7dfd4a3d96d14275881cf42
Jaroslaw Kolasa <kolasa.jarek@gmail.com>
2019-06-19 05:37:05 +0200

    PredkoscSrednia and PredkoscSredniaTests added

--- spoj/PredkoscSrednia.cs
+++ spoj/PredkoscSrednia.cs
@@ -6,21 +6,23 @@ using System.Threading.Tasks;
 
 namespace spoj
 {
-    class Program
+    public class PredkoscSrednia
     {
-        static void Main(string[] args)
-        {
             int tests, v1, v2;
-            string input;
             string[] inputArr;
 
-            tests = int.Parse(Console.ReadLine());
-            
-            input = Console.ReadLine();
+        public PredkoscSrednia(string input)
+        {
             inputArr = input.Split(' ');
+            tests = inputArr.Length;
+
             v1 = int.Parse(inputArr[0]);
             v2 = int.Parse(inputArr[1]);
-            Console.WriteLine((2*v1*v2)/(v1 + v2));
+        }
+
+    public int GetAverageSpeed()
+        {
+            return (2 * v1 * v2) / (v1 + v2);
         }
     }
 }
---
+++ spojTests/PredkoscSredniaTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spoj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoj.Tests
+{
+    [TestClass()]
+    public class PredkoscSredniaTests
+    {
+        [TestMethod()]
+        public void ReturnTrueTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void AverageSpeed5050ShouldGive50Test()
+        {
+            string inputSpeed = "50 50"; 
+            int vavrg = 50;
+            PredkoscSrednia predkosc = new PredkoscSrednia(inputSpeed);
+            Assert.AreEqual(vavrg, predkosc.GetAverageSpeed());
+        }
+
+        [TestMethod()]
+        public void AverageSpeed4060ShouldGive48Test()
+        {
+            string inputSpeed = "40 60";
+            int vavrg = 48;
+            PredkoscSrednia predkosc = new PredkoscSrednia(inputSpeed);
+            Assert.AreEqual(vavrg, predkosc.GetAverageSpeed());
+        }
+    }
+}
