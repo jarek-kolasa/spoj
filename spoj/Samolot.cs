f49e2e129ca5df0ab8cfd59de661248880dcbe7c
Jaroslaw Kolasa <kolasa.jarek@gmail.com>
2019-07-02 05:23:55 +0200

    Samolot class added and solved

---
+++ spoj/Samolot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoj
+{
+    public class Samolot
+    {
+        public int GetSits(string input)
+        {
+            string[] inputArr = input.Split(' ');
+
+            int rowsBuiss = int.Parse(inputArr[0]);
+            int colBuiss = int.Parse(inputArr[1]);
+            int rowsEconomic = int.Parse(inputArr[2]);
+            int colEconomic = int.Parse(inputArr[3]);
+
+            return rowsBuiss * colBuiss + rowsEconomic * colEconomic;
+        }
+    }
+}
---
+++ spojTests/SamolotTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spoj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoj.Tests
+{
+    [TestClass()]
+    public class SamolotTests
+    {
+        [TestMethod()]
+        public void GetSitsTestPass()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void Get70SitsTest()
+        {
+            string text = "2 5 3 20";
+            int output = 70;
+
+            Samolot samolot = new Samolot();
+
+            Assert.AreEqual(output, samolot.GetSits(text));
+        }
+    }
+}
